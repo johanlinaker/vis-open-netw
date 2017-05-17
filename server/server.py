c983d0e4c465c7f9e6170777b4afe5e0a8a8d3a6
ky4843ca <ky4843ca@exjobb19.cs.lth.se>
2017-05-17 16:36:31 +0200

    initial commit for python server - rough code

---
+++ server/server.py
@@ -0,0 +1,61 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from SocketServer import ThreadingMixIn
+import threading
+import argparse
+import re
+
+
+class LocalData(object):
+    records = {}
+
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Credentials', 'true')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        file = open("../visualizationProject/public_html/Data/R2.2.0_comments.json")
+        self.wfile.write(file.read())
+        return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    allow_reuse_address = True
+
+    def shutdown(self):
+        self.socket.close()
+        HTTPServer.shutdown(self)
+
+
+class SimpleHttpServer():
+    def __init__(self, ip, port):
+        self.server = ThreadedHTTPServer((ip, port), HTTPRequestHandler)
+
+    def start(self):
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread.daemon = True
+        self.server_thread.start()
+
+    def waitForThread(self):
+        self.server_thread.join()
+
+    def addRecord(self, recordID, jsonEncodedRecord):
+        LocalData.records[recordID] = jsonEncodedRecord
+
+    def stop(self):
+        self.server.shutdown()
+        self.waitForThread()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='HTTP Server')
+    parser.add_argument('port', type=int, help='Listening port for HTTP Server')
+    parser.add_argument('ip', help='HTTP Server IP')
+    args = parser.parse_args()
+
+    server = SimpleHttpServer(args.ip, args.port)
+    print 'HTTP Server Running...........'
+    server.start()
+    server.waitForThread()
